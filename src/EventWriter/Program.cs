225e46d93a2bd9c7670368564ca8af3f11e46db6
jeremy woo <kepungmoin@hotmail.com>
2019-05-30 19:55:36 +1200

    init commit

---
+++ src/EventWriter/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Azure.EventHubs;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+ 
+namespace EventWriter
+{
+    class Program
+    {
+
+        private static EventHubClient eventHubClient;
+        private const string EventHubConnectionString = "{Event Hubs connection string}";
+        private const string EventHubName = "{Event Hub path/name}";
+
+
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Hello World!");
+            SendAsync().GetAwaiter().GetResult();
+        }
+
+
+        private static async Task SendAsync()
+        {
+            var client = EventHubClient.CreateFromConnectionString(EventHubConnectionString);
+            
+            for (int i = 0; i < 100; i++)
+            {
+                 byte[] dataMessage = Encoding.UTF8.GetBytes("Sending fake message" + i);
+
+                var fakeEventMessage = new EventData(dataMessage);
+                await  client.SendAsync(fakeEventMessage);
+            }
+        }
+     }
+}
