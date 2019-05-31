f4d2987c84d99e48c49115d229a98fa9465dce13
jeremy woo <kepungmoin@hotmail.com>
2019-05-31 18:33:52 +1200

    updated support for different containers

--- src/EventReader/Program.cs
+++ src/EventReader/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using System.Threading.Tasks;
+
 
 namespace EventReader
 {
     class Program
     {
+
+       private const string EventHubConnectionString = "Endpoint=sb://testehnstempdata.servicebus.windows.net/;SharedAccessKeyName=testapppolicy;SharedAccessKey=edt0qBfK7Tu3nPqNEDl2OHqhaTI0dgeH16GL5cJg6kI=;EntityPath=ehtesttargetapp";
+        private const string EventHubName = "ehtesttargetapp";
+        private const string StorageContainerName = "";
+        private const string StorageAccountName = "sateststore";
+        private const string StorageAccountKey = "E9Z0BUTEbahjyKRoBnCU/3XI3cxv2MynY3U+UuFMuwFVH5hE0w2tGN3N5rS/C5rjx9Ekmvz/AQtUVH4h3dpt2w==";
+   
+        private const string StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=sateststore;AccountKey=E9Z0BUTEbahjyKRoBnCU/3XI3cxv2MynY3U+UuFMuwFVH5hE0w2tGN3N5rS/C5rjx9Ekmvz/AQtUVH4h3dpt2w==;EndpointSuffix=core.windows.net";
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!", args[0]);
+
+            ReadEventData(args[0]).GetAwaiter().GetResult();
+        }
+
+        private static async Task ReadEventData(string containerName)         
+        {
+
+            Console.WriteLine($"Registering EventProcessor...to {containerName}");
+
+            var eventProcessorHost = new EventProcessorHost(
+                EventHubName,
+                PartitionReceiver.DefaultConsumerGroupName,
+                EventHubConnectionString,
+                StorageConnectionString,
+                containerName);
+
+            // Registers the Event Processor Host and starts receiving messages
+            await eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>();
+
+            Console.WriteLine("Receiving. Press ENTER to stop worker.");
+            Console.ReadLine();
+
+            // Disposes of the Event Processor Host
+            await eventProcessorHost.UnregisterEventProcessorAsync();
+
+
         }
     }
 }
